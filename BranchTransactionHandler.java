b9e75b82768833bd669162d93b29c48e0bfc2195
hadleym <hadleym@email.arizona.edu>
2017-04-30 18:06:14 -0700

    BranchTransactionHandler can handle hard coded remote servers.

--- BranchTransactionHandler.java
+++ BranchTransactionHandler.java
@@ -1,26 +1,38 @@
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
 class BranchTransactionHandler {
 	Ledger ledger;
+	String otherAddr;
+	int otherPort;
 	public BranchTransactionHandler ( Ledger l ){
-		ledger = l;	
+		ledger = l;
+        otherAddr = "127.0.0.1";
+        otherPort = 9090;
 	}
 	public Transaction handleTransaction(Transaction t){
+
 		Account a = ledger.getAccount(t.getId());
 		if ( a != null){
 			if ( t.getAction() == Action.BALANCE_INQUIRY ){
-				return handleBalanceInquiry((BalanceInquiry) t);
+				t = handleBalanceInquiry((BalanceInquiry) t);
 			} else if ( t.getAction() == Action.WITHDRAW ){
-				return handleWithdraw((Withdraw) t, a);
+				t = handleWithdraw((Withdraw) t, a);
 			} else if ( t.getAction() == Action.DEPOSIT ) {
-                return handleDeposit((Deposit) t, a);
-        } else {
-				System.out.println("This type of transaction cannot be processed by a branch (this error should not appear, please contact administrator.)");
-				t.setStatus(Status.FAILURE);
-				return t;
-			}
-		} else {
+                t = handleDeposit((Deposit) t, a);
+            }
+           return t;
+   		} else {
             if ( t.getAction() == Action.CLOSE) {
                 return handleCloseTransaction((CloseTransaction) t);
+
             } else {
+                t = handleAnotherServerTransaction(t);
+                if ( t.getStatus() == Status.SUCCESS){
+                    return t;
+                }
 				System.out.println("Transaction cannot be processed at this branch. " + t.getId());
 			    t.setStatus(Status.FAILURE);
             }
@@ -64,5 +76,38 @@ class BranchTransactionHandler {
 		c.setStatus(Status.SUCCESS);
 		return c;
 	}
+
+	private Transaction handleAnotherServerTransaction(Transaction t){
+	    try {
+            Socket socket = new Socket(otherAddr, otherPort);
+            sendTransaction(t, socket);
+            t = recvTransaction(socket);
+        } catch( IOException e){
+	        System.out.println("Transaction handler couldnt connect to remote branch");
+	        e.printStackTrace();
+        }
+        return t;
+
+    }
+
+    public void sendTransaction(Transaction trans, Socket socket) throws IOException  {
+        ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
+        System.out.println( this + " Sending transaction of type " + trans.getAction());
+        out.writeObject(trans);
+        System.out.println( this + " transaction sent.");
+    }
+    public Transaction recvTransaction(Socket socket) throws IOException {
+
+        System.out.println( this + " receiving transation...");
+        Transaction trans = null;
+        try {
+            ObjectInputStream input = new ObjectInputStream(socket.getInputStream());
+            trans = (Transaction) input.readObject();
+            System.out.println( this + " received transaction");
+        } catch (ClassNotFoundException e){
+            e.printStackTrace();
+        }
+        return trans;
+    }
 }
 	
